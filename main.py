e8697e41fff2239902c40c45ae727dfbc965e347
nakashima-wataru <seidannnw@gmail.com>
2025-11-18 11:14:12 +0900

    ui 1

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-def main():
-    print("Hello from b-25-finance!")
+from shiny import App, render, ui
 
+app_ui = ui.page_fluid(
+    ui.input_selectize(  
+        "selectize",  
+        "Select an option below:",  
+        {"1A": "Choice 1A", "1B": "Choice 1B", "1C": "Choice 1C"},  
+    ),  
+    ui.output_text("value"),
+)
 
-if __name__ == "__main__":
-    main()
+def server(input, output, session):
+    @render.text
+    def value():
+        return f"{input.selectize()}"
 
-print(0)
+app = App(app_ui, server)
